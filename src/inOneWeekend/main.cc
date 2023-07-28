a86a573adad960b5150631bdd7c41ae94946f5fc
quanzw99 <carry_quan@163.com>
2023-07-27 21:54:59 -0400

    update for 6.1 version

--- src/inOneWeekend/main.cc
+++ src/inOneWeekend/main.cc
@@ -3,20 +3,33 @@
 #include "ray.h"
 #include "vec3.h"
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
     auto b = 2.0 * dot(oc, r.direction());
     auto c = dot(oc, oc) - radius * radius;
     auto discriminant = b * b - 4 * a * c;
-    return (discriminant > 0);
+
+    // calculate the smallest t
+    if (discriminant < 0.0) {
+        return -1.0;
+    }
+    else {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r))
-        return color(1.0, 0.0, 0.0);
+
+    // point3(0, 0, -1) is the center of the sphere
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 N = unit_vector(r.at(t) - point3(0, 0, -1));
+        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+    }
+
     vec3 unit_direction = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direction.y() + 1.0);
+    t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
@@ -25,7 +38,7 @@ int main() {
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
-    const int image_height = image_width / aspect_ratio;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
 
     // Camera
     auto viewport_height = 2.0;
