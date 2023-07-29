92c88ba38c0df0d087ef5f7c39c1dd773d6e78d3
quanzw99 <carry_quan@163.com>
2023-07-29 14:58:09 -0400

    Front Faces Versus Back Faces

--- src/inOneWeekend/hittable.h
+++ src/inOneWeekend/hittable.h
@@ -8,6 +8,12 @@ struct hit_record {
     point3 p;    // intersection point
     vec3 normal; // the unit surface normal vector at the intersection point 
     double t;    // parameter t at the intersection point
+    bool front_face; // the direction of the normal
+
+    inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
 
 class hittable {
