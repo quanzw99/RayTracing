6b5ab888e0d3e9d3f30f33c7b933e33d78a8733b
quanzw99 <carry_quan@163.com>
2023-07-30 02:01:57 -0400

    Common Constants and Utility Functions

--- src/inOneWeekend/main.cc
+++ src/inOneWeekend/main.cc
@@ -1,62 +1,32 @@
 #include <iostream>
-#include <memory>
 
-using std::shared_ptr;
-using std::make_shared;
-
+#include "rtweekend.h"
 #include "color.h"
-#include "ray.h"
-#include "vec3.h"
 #include "sphere.h"
+#include "hittable_list.h"
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
-    vec3 oc = r.origin() - center;
-    auto a = r.direction().length_squared();
-    auto half_b = dot(oc, r.direction());
-    auto c = oc.length_squared() - radius * radius;
-    auto discriminant = half_b * half_b - a * c;
-
-    // calculate the smallest t
-    if (discriminant < 0.0) {
-        return -1.0;
-    }
-    else {
-        return (-half_b - sqrt(discriminant)) / a;
-    }
-}
-
-color ray_color(const ray& r) {
-
-    // point3(0, 0, -1) is the center of the sphere
-    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
-    if (t > 0.0) {
-        vec3 N = unit_vector(r.at(t) - point3(0, 0, -1));
-        return 0.5 * color(N.x() + 1, N.y() + 1, N.z() + 1);
+color ray_color(const ray& r, const hittable& world) {
+    hit_record rec;
+    if (world.hit(r, 0, infinity, rec)) {
+        return 0.5 * (rec.normal + color(1, 1, 1));
     }
 
     vec3 unit_direction = unit_vector(r.direction());
-    t = 0.5 * (unit_direction.y() + 1.0);
+    auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
-void test_shared_point() {
-    shared_ptr<double> double_ptr = make_shared<double>(0.37);
-    shared_ptr<vec3> vec3_ptr     = make_shared<vec3>(1.414214, 2.718281, 1.618034);
-    shared_ptr<sphere> sphere_ptr = make_shared<sphere>(point3(0, 0, 0), 1.0);
-
-    auto double_ptr1 = make_shared<double>(0.37);
-    auto vec3_ptr1   = make_shared<vec3>(1.414214, 2.718281, 1.618034);
-    auto sphere_ptr1 = make_shared<sphere>(point3(0, 0, 0), 1.0);
-}
-
 int main() {
-
-    test_shared_point();
 
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // World
+    hittable_list world;
+    world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+    world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
     // Camera
     auto viewport_height = 2.0;
@@ -77,7 +47,7 @@ int main() {
             auto u = double(i) / image_width;
             auto v = double(j) / image_height;
             ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-            color pixel_color = ray_color(r);
+            color pixel_color = ray_color(r, world);
             write_color(std::cout, pixel_color);
         }
     }
